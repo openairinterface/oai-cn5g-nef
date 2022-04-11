f6309a43a199091092af66c72969e362f939d34c
Raphael Defosseux <raphael.defosseux@openairinterface.org>
2022-04-11 10:49:51 +0200

    feat(ci): adding a simple sanity check deployment for validating image fullness
    Signed-off-by: Raphael Defosseux <raphael.defosseux@openairinterface.org>

--- ci-scripts/sanityCheckDeploy.py
+++ ci-scripts/sanityCheckDeploy.py
@@ -20,12 +20,16 @@
 """
 
 import argparse
+import os
+import re
+import subprocess
 import sys
+import time
 
 def main() -> None:
     args = _parse_args()
-    print (f'Does nothing for the moment on {args.tag}')
-    sys.exit(0)
+    status = generic_deployment(args.tag)
+    sys.exit(status)
 
 def _parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description='Sanity Check')
@@ -37,6 +41,101 @@ def _parse_args() -> argparse.Namespace:
         help='Image Tag in image-name:image tag format',
     )
     return parser.parse_args()
+
+def generic_deployment(tag):
+    # First detect which docker/podman command to use
+    cli = ''
+    cmd = 'which podman || true'
+    podman_check = subprocess.check_output(cmd, shell=True, universal_newlines=True)
+    if re.search('podman', podman_check.strip()):
+        cli = 'sudo podman'
+    if cli == '':
+        cmd = 'which docker || true'
+        docker_check = subprocess.check_output(cmd, shell=True, universal_newlines=True)
+        if re.search('docker', docker_check.strip()):
+            cli = 'docker'
+    if cli == '':
+        print ('No docker / podman installed: quitting')
+        return -1
+
+    status = 0
+    if cli == 'docker':
+        infos = tag.split(':')
+        cmd = 'sed -e "s@NEF_IMAGE_TAG@' + infos[1] + '@" ci-scripts/docker-compose/docker-compose.tplt > ci-scripts/docker-compose/docker-compose.yaml'
+        print (cmd)
+        subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True, universal_newlines=True)
+        cmd = 'cd ci-scripts/docker-compose && docker-compose up -d'
+        print (cmd)
+        upStatus = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True, universal_newlines=True)
+        for line in str(upStatus.strip()).split('\n'):
+            print(line)
+        time.sleep(20)
+        cmd = 'docker inspect --format="STATUS: {{.State.Health.Status}}" cicd-oai-nef'
+        print (cmd)
+        healthStatus = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True, universal_newlines=True)
+        for line in str(healthStatus.strip()).split('\n'):
+            print(line)
+            if re.search('STATUS:', line):
+                if re.search('STATUS: healthy', line):
+                    status = 0
+                else:
+                    status = -1
+        cmd = 'docker logs cicd-oai-nef'
+        print (cmd)
+        logStatus = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True, universal_newlines=True)
+        for line in str(logStatus.strip()).split('\n'):
+            print(line)
+        time.sleep(5)
+        cmd = 'cd ci-scripts/docker-compose && docker-compose down'
+        print (cmd)
+        downStatus = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True, universal_newlines=True)
+        for line in str(downStatus.strip()).split('\n'):
+            print(line)
+        cmd = 'docker volume prune --force || true'
+        subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True, universal_newlines=True)
+    else:
+        cmd = 'sudo podman network create --subnet 192.168.28.192/26 --ip-range 192.168.28.192/26 cicd-oai-public-net'
+        print (cmd)
+        netUpStatus = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True, universal_newlines=True)
+        for line in str(netUpStatus.strip()).split('\n'):
+            print(line)
+        time.sleep(1)
+        cwd = os.getcwd()
+        cmd = 'sudo podman run --name cicd-oai-nef --network cicd-oai-public-net --ip 192.168.28.210'
+        cmd += ' --volume ' + cwd + '/ci-scripts/docker-compose/nef-healthy-check.sh:/openair-nef/bin/nef-healthy-check.sh'
+        cmd += ' --health-cmd /openair-nef/bin/nef-healthy-check.sh'
+        cmd += ' --env-file ./ci-scripts/podman/nef.env -d ' + tag
+        print (cmd)
+        contUpStatus = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True, universal_newlines=True)
+        for line in str(contUpStatus.strip()).split('\n'):
+            print(line)
+        time.sleep(20)
+        cmd = 'sudo podman inspect --format="STATUS: {{.State.Healthcheck.Status}}" cicd-oai-nef'
+        print (cmd)
+        healthStatus = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True, universal_newlines=True)
+        for line in str(healthStatus.strip()).split('\n'):
+            print(line)
+            if re.search('STATUS:', line):
+                if re.search('STATUS: healthy', line):
+                    status = 0
+                else:
+                    status = -1
+        cmd = 'sudo podman logs cicd-oai-nef'
+        print (cmd)
+        logStatus = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True, universal_newlines=True)
+        for line in str(logStatus.strip()).split('\n'):
+            print(line)
+        cmd = 'sudo podman rm -f cicd-oai-nef'
+        print (cmd)
+        contDwnStatus = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True, universal_newlines=True)
+        for line in str(contDwnStatus.strip()).split('\n'):
+            print(line)
+        cmd = 'sudo podman network rm cicd-oai-public-net'
+        print (cmd)
+        netDwnStatus = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True, universal_newlines=True)
+        for line in str(netDwnStatus.strip()).split('\n'):
+            print(line)
+    return status
 
 if __name__ == '__main__':
     main()
